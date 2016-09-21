77f3ee2323000a9e3a8648b32360b0a3ddc2bf39
Edu Martin <emartinpicado@gmail.com>
2016-09-21 16:54:07 +0200

    feat(*): more configuration properties

--- polymer-twitter-statictimeline.js
+++ polymer-twitter-statictimeline.js
@@ -3,15 +3,68 @@
     is: 'polymer-twitter-statictimeline',
 
     properties: {
+
+      /**
+       * This id has to be created through twitter account
+       */
       widgetId: String,
-      ariaPolite: String, //polite, assertive, rude
+
+      /**
+       * Set an assertive ARIA politeness value for widget components and updates.
+       * *polite*, *assertive* or *rude*
+       * Default *polite*
+       */
+      ariaPolite: String,
+
+      /**
+       * When set to dark, displays Tweet with light text over a dark background.
+       */
+      theme: String,
+
+      /**
+       * Adjust the color of Tweet links with a hexadecimal color value.
+       */
+      linkColor: String,
+
+      /**
+       * Set the height of a displayed widget, overriding the value stored with the widget ID. Must be greater than 200 pixels.
+       * Note: the height parameter does not apply when a tweet-limit parameter is specified
+       * Default: *600*
+       */
       height: Number,
+
+      /**
+       * Set the maximum width of the widget between 180 and 520 pixels.
+       */
+      width: Number,
+
+      /**
+       * Set the color of widget component borders, including the border between Tweets, with a hexadecimal color value.
+       */
       borderColor: String,
-      chrome: String, //noheader, nofooter, noborders, transparent, noscrollbar (space-separated list)
+
+      /**
+       *
+       * Remove a display component of a timeline with space-separated tokens:
+       *  *noheader* - hides the header
+       *  *nofooter* - hides the footer, if visible
+       *  *noborders* - removes all borders: around the widget, between Tweets, and inside a Tweet
+       *  *noscrollbar* - crop and hide the timeline scrollbar, if visible
+       *  *transparent* - remove background color
+       */
+      chrome: String,
+
+      /**
+       * Display an expanded timeline of between 1 and 20 Tweets.
+       */
       tweetLimit: Number,
-      showReplies: {
-        type: Boolean,
-        value: false
+
+      /**
+       * A supported Twitter language code.
+       */
+      lang: {
+        type: String,
+        value: 'en'
       }
     },
 
@@ -20,24 +73,29 @@
         this._log(this._logf('widgetId', 'widgetId must not be undefined'));
         return;
       }
-
       twttr.ready(function() {
-        this.createWidget();
+        this._createWidget();
       }.bind(this));
     },
 
-    createWidget: function() {
+    /**
+     * Create a twitter widget with a specific id
+     */
+    _createWidget: function() {
       twttr.widgets.createTimeline({
         sourceType: 'widget',
         widgetId: this.widgetId
       },
       this.$.timeline, {
         ariaPolite: this.ariaPolite,
-        height: this.height,
+        lang: this.lang,
+        theme: this.theme,
+        linkColor: this.linkColor,
         borderColor: this.borderColor,
+        height: this.height,
+        width: this.width,
         chrome: this.chrome,
-        tweetLimit: this.tweetLimit,
-        showReplies: this.showReplies
+        tweetLimit: this.tweetLimit
       });
     }
   });
